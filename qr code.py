6ade915eb5596bb2db9876425595fa96adbf931c
Peter Kanyi <118138496+Peter-Kanyi@users.noreply.github.com>
2023-01-03 16:59:37 +0100

    Update qr code.py

--- qr code.py
+++ qr code.py
@@ -1,26 +1,28 @@
-import pyqrcode
+#Let's import the required libraries
+import pyqrcode #for generating the QR code
 import png
 from PIL import ImageTk, Image
 from pyqrcode import QRCode
-import tkinter as tk 
+import tkinter as tk #for creating the gui window
 from tkinter import *
 import PIL.Image
-
-window = Tk()  
-window.geometry('300x350')
-window.title('QR Code generator by Kanyi')
+#Next, we create the GUI window
+window = Tk()  #creates a GUI window called Window
+window.geometry('300x350') #Specifies the size of the window
+window.title('QR Code generator by Kanyi') #the title of the window
 
 Label(window,text='Copy Paste your link to create a QR Code',font='arial 15').pack()
 # String which represents the QR code
 s = tk.StringVar()
   
-# Generate QR code
+#QRCode generation function
 def create_qrcode():
     s1=s.get()
     qr = pyqrcode.create(s1)
     qr.png('myqr.png', scale = 6)
     Label(window,text='Your QR Code has been created and saved').pack()
 
+    #Button and Entry Field
 Entry(window,textvariable=s,font='arial 15').pack()
 Button(window,text='create',bg='green',command=create_qrcode).pack()
 window.mainloop()
